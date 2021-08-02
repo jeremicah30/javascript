c2d3ffd445745807f395b18258a18a111787eb1d
jeremicah30 <maigabie17@gmail.com>
2021-08-02 22:25:49 +0800

    First

---
+++ Arrow Functions/script.js
@@ -0,0 +1,15 @@
+// const hiThere = function(){
+//     console.log('This is a es5 function');
+// }
+
+//Arrow
+// const hiThere = () => {
+//     console.log('This is an arrow function');
+// }
+
+// const hiThere = () => console.log('This is a one line arrow function');
+
+//Return
+const hiThere = () => 'This is a return function'
+
+console.log(hiThere());
