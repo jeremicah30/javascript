02f7685852c0b2fd081802e07408112af03aea03
jeremicah30 <maigabie17@gmail.com>
2021-08-03 18:25:59 +0800

    Change code in ES6

--- Arrow Functions/script.js
+++ Arrow Functions/script.js
@@ -3,13 +3,27 @@
 // }
 
 //Arrow
-// const hiThere = () => {
-//     console.log('This is an arrow function');
-// }
+    // const hiThere = () => {
+    //     console.log('This is an arrow function');
+    // }
 
-// const hiThere = () => console.log('This is a one line arrow function');
+    // const hiThere = () => console.log('This is a one line arrow function');
 
 //Return
-const hiThere = () => 'This is a return function'
+    // const hiThere = function () {
+    //     return 'this is a return function'
+    // }
 
-console.log(hiThere());
+    // const hiThere = () => 'This is a one line return function'
+
+//Return Object
+    // const hiThere = () => ({message: 'This is a return object'})
+
+//Parameter --Single parameter does not neet paranthesis
+    // const hiThere = name => console.log(`Hi there this ${name}`)
+
+//Parameter --More than one parameter needs parenthesis
+    const hiThere = (firstName, lastName) => console.log(`Hi There ${firstName} ${lastName}`)
+
+
+hiThere('Efraim', 'Gabuat');
--- Fetch API/script.js
+++ Fetch API/script.js
@@ -3,27 +3,81 @@ document.getElementById('button-2').addEventListener('click', getJson);
 document.getElementById('button-3').addEventListener('click', getExApi);
 
 //Get Text
+// function getText() {
+//     fetch('sample.txt')
+//     .then(function(response) {
+//         return response.text();
+//     })
+//     .then(function(data) {
+//         console.log(data);
+//         document.getElementById('output').innerHTML = data;
+//     })
+//     .catch(function(err) {
+//         console.log(err);
+//     })
+// };
+
+//Get JSON 
+    // function getJson() {
+    //     fetch('samp.json')
+    //     .then(function(response) {
+    //         return response.json();
+    //     })
+    //     .then(function(data) {
+    //         console.log(data);
+
+    //         let output = '';
+    //         data.forEach(function(post) {
+    //             output += `<li>${post.title}</li>`;
+
+    //             document.getElementById('output').innerHTML = output;
+    //         })
+    //     })
+    //     .catch(function(err) {
+    //         console.log(err);
+    //     })
+    // };
+
+//Get external API
+// function getExApi() {
+//     fetch('https://api.github.com/users')
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         console.log(data);
+
+//         let output = '';
+//         data.forEach(function(data) {
+//             output += `<li>${data.login}</li>`;
+
+//             document.getElementById('output').innerHTML = output;
+//         })
+//     })
+//     .catch(function(err) {
+//         console.log(err);
+//     })
+// }
+
+
+//------------------------ES6 VERSION--------------------------------
+
+//GET TEXT
 function getText() {
     fetch('sample.txt')
-    .then(function(response) {
-        return response.text();
-    })
-    .then(function(data) {
+    .then(response => response.text())
+    .then(data => {
         console.log(data);
         document.getElementById('output').innerHTML = data;
     })
-    .catch(function(err) {
-        console.log(err);
-    })
+    .catch(err => console.log(err))
 };
 
-//Get JSON
+//Get JSON ES6
 function getJson() {
     fetch('samp.json')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
+    .then(response => response.json())
+    .then(data => {
         console.log(data);
 
         let output = '';
@@ -33,18 +87,14 @@ function getJson() {
             document.getElementById('output').innerHTML = output;
         })
     })
-    .catch(function(err) {
-        console.log(err);
-    })
+    .catch(err => console.log(err));
 };
 
 //Get external API
 function getExApi() {
     fetch('https://api.github.com/users')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
+    .then(response => response.json())
+    .then(data => {
         console.log(data);
 
         let output = '';
@@ -54,7 +104,5 @@ function getExApi() {
             document.getElementById('output').innerHTML = output;
         })
     })
-    .catch(function(err) {
-        console.log(err);
-    })
+    .catch(err => console.log(err))
 }
